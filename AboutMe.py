961ba78ed3696a47378945a8e5a1e875b662379d
Anand Sahu <anandsahu5097@gmail.com>
2024-11-25 17:40:52 +0530

    Added Updated Resume error solving

--- AboutMe.py
+++ AboutMe.py
@@ -6,7 +6,7 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "ANAND_11_2024.pdf"
+resume_file = current_dir / "assets" / "ANAND.2024.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
 
 
