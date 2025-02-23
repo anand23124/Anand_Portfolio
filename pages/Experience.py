04bfbcd782da77785af8936920c0dd4177f92296
Anand Sahu <anandsahu5097@gmail.com>
2025-02-23 19:53:53 +0530

    Updated the Resume and added the new work experience

--- pages/Experience.py
+++ pages/Experience.py
@@ -5,18 +5,31 @@ st.title("Work Experience")
 st.write("---")
 
 # --- JOB 1 ---
-st.write("🚧", "**Machine Learning Engineer Intern | Nuacem AI, Hyderabad**")
-st.write("06/2024 - 11/2024")
+st.write("🚧", "**Data Scientist | Cogninest AI, Pune**")
+st.write("12/2024 - Present")
 st.write(
     """
-    - Gained insights into advanced tools in machine learning, applicable to solving real-world problems.
-    - Learned to leverage different tools to address complex ML challenges by harnessing the power of AI.
-    - Developed various projects such as a Custom Dataset AI Chatbot and an Image Information Extractor.
+    - Developed and deployed **end-to-end time series forecasting pipelines**, optimizing **data preprocessing, model training, and deployment**.
+    - Automated **data collection and workflow integration** using **Zapier**, improving efficiency and reducing manual effort.
+    - Built **scalable ML models** for real-world business problems with **cloud-based deployment**.
+    - Applied **MLOps best practices**, ensuring **version control, monitoring, and CI/CD integration**.
     """
 )
 
 # --- JOB 2 ---
-st.write('\n')
+st.write("\n")
+st.write("🚧", "**Machine Learning Engineer Intern | Nuacem AI, Hyderabad**")
+st.write("06/2024 - 12/2024")
+st.write(
+    """
+    - Built an **AI chatbot** to retrieve information from **PDF, DOCX, and text files** using **LLMs**.
+    - Developed an **automated tool** using **Google’s Vision LLM** to extract and classify **error codes from images**.
+    - Created an **AI-powered chatbot** to **analyze, compare, and highlight differences** between documents using **LLM-based NLP**.
+    """
+)
+
+# --- JOB 3 ---
+st.write("\n")
 st.write("🚧", "**AIML/IOT Trainee | Code Unnati, Surat**")
 st.write("01/2024 - 04/2024")
 st.write(
